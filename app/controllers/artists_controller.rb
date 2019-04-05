94d35e1ce100f68b14f931b7c9d2b4160e9cdc88
Samantha brake <sammy.brake@gmail.com>
2019-04-05 22:24:06 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,4 +8,9 @@ class ArtistsController < ApplicationController
   def new
     @artist = Artist.new
   end
+
+  def create
+    @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
 end
