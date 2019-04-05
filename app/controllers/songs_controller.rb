8ab27f62037d560fa90f07b322f8f3913015a48c
Samantha brake <sammy.brake@gmail.com>
2019-04-05 22:35:40 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,16 @@ class SongsController < ApplicationController
   def show
     @song = Song.find_by(params[:id])
   end
+  
   def index
   end
+
+  def new 
+    @song = Song.new
+  end 
+
+  def create 
+    @song = Song.create(params.require(:song).permit(:name))
+    redirect_to song_path(@song)
+  end 
 end
