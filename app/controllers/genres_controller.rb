635820024a9cba05dfbc934c1e01c2019cd69a57
Samantha brake <sammy.brake@gmail.com>
2019-04-05 22:29:09 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,4 +4,12 @@ class GenresController < ApplicationController
     @genre = Genre.find_by(params[:id])
   end
 
+  def new 
+    @genre = Genre.new
+  end 
+
+  def edit 
+      @genre = Genre.find_by(params[:id])
+  end 
+
 end
